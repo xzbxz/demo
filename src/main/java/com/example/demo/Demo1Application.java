8473f5395d1cda774f23bc1379d07cf155432ff0
Hmdily <1019200553@qq.com>
2023-03-10 14:18:21 +0800

    tijiao 2

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo1Application {
 
     public static void main(String[] args) {
+
+        System.out.println("hahaha");
         SpringApplication.run(Demo1Application.class, args);
+
+
     }
 
 }
